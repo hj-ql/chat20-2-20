2dd4df3fcbf852a61ab1594aa770d23443f02b83
1778699668 <1778699668@qq.com>
2020-02-20 10:48:51 +0800

    上传基线代码

---
+++ 4 volume  project   ChatClient1_1/4 volume  project   ChatClient1_1Dlg.h
@@ -0,0 +1,38 @@
+﻿
+// 4 volume  project   ChatClient1_1Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMy4volumeprojectChatClient11Dlg 对话框
+class CMy4volumeprojectChatClient11Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMy4volumeprojectChatClient11Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY4VOLUMEPROJECTCHATCLIENT1_1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLbnSelchangeList1();
+	afx_msg void OnBnClickedClearmsg();
+	afx_msg void OnBnClickedSendBtn();
+};
---
+++ 4 volume  project   ChatServer1_2/4 volume  project   ChatServer1_2Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// 4 volume  project   ChatServer1_2Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMy4volumeprojectChatServer12Dlg 对话框
+class CMy4volumeprojectChatServer12Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMy4volumeprojectChatServer12Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY4VOLUMEPROJECTCHATSERVER1_2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
